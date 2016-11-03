3d91bdcd5305af94840df27ae54ec8ce532f62a8
Javier <javier.su.weijie@gmail.com>
2016-11-03 15:31:15 +0000

    changed luis endpoint

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var Asteroid = require('asteroid').createClass();
+var ws = require('ws');
 
 //=========================================================
 //// Bot Setup
@@ -11,12 +13,18 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
+//// Setup asteroid to update database
+var asteroid = new Asteroid({
+    endpoint: 'ws://localhost:3000/websocket',
+    SocketConstructor: ws
+});
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: "c20ed0f2-619f-4eeb-b121-770a070574b6",
     appPassword: "N269inrXkenPWcqAqnitMRs"
 });
-var model = 'https://api.projectoxford.ai/luis/v1/application?id=45e40ce1-29d7-42cb-9c11-5785f9eefbd4&subscription-key=8f04842c92b84e25940c3fa2cea35472'
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=de768df7-e6e5-4b47-a6fe-79c3f286da6e&subscription-key=e01c03755775464e8028a0aab1940872'
 var recognizer = new builder.LuisRecognizer(model);
 var bot = new builder.UniversalBot(connector);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
@@ -43,6 +51,7 @@ dialog.matches('find_industry', function(session, args) {
     var industry = builder.EntityRecognizer.findEntity(args.entities, 'industry');
     console.log(industry);
     session.send("Searching for %s", industry.entity);
+    asteroid.call('createCompany', "new company");
 });
 
 dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
