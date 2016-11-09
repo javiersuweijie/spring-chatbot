5f5dd8669a582d2b0c0ca09cd26ff1e0487381e8
sharonczy <sharonczy@gmail.com>
2016-11-10 04:01:58 +0800

    added thanks randomisation

---
+++ thanks.js
@@ -0,0 +1,13 @@
+const random = array => { return array[Math.floor(Math.random() * array.length)] };
+
+const thanksMsg = () => {
+  const answers = [
+    "Thanks!",
+    "Thank you! :)",
+    "Aww, that's nice of you.",
+    "Thanks! That means a lot."
+  ]
+  return random(answers)
+};
+
+module.exports = thanksMsg;
