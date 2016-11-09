885453c860b1b4d422bcedbf606d3ac05dbd6298
sharonczy <sharonczy@gmail.com>
2016-11-09 09:58:14 +0800

    fixed investor flow again

--- app.js
+++ app.js
@@ -105,9 +105,31 @@ dialog.matches('hello', function(session) {
     session.beginDialog('/profile');
 });
 
-dialog.matches('find_investees', function(session) {
-    session.beginDialog('/investees');
-});
+dialog.matches('find_investees', [
+    function(session, args, next) {
+        var industry = builder.EntityRecognizer.findEntity(args.entities, 'sector');
+        console.log(industry);
+        if (!industry) {
+            builder.Prompts.text(session, 'Which industry are you interested in?');
+        }
+        else {
+            next({ response: industry.entity});
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+// note to self: should do a filter, then do a count.
+// if count = 0, need to say that there are no results found. if count > 0, can display number of entries as well
+        session.send("Roger that. Displaying %s entities on your screen.", results.response);
+        asteroid.call('setFilter', {sector: results.response.trim()});
+        session.send("Do you have more requirements (e.g. company age, investment till date)?");
+        }
+        else {
+            session.send("I don't understand.");
+            session.endDialog();
+        }
+    }
+]);
 
 // competition criteria and small talk dialogs
 
