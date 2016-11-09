a822cd21bf6c762fb799380fd42b6e7af9abc9d3
Javier <javier.su.weijie@gmail.com>
2016-11-09 08:50:03 +0000

    removed choice

--- profile.js
+++ profile.js
@@ -3,7 +3,8 @@
 startup = function(bot, builder, asteroid) {
     bot.dialog('/profile', [
         function (session, results) {
-            builder.Prompts.choice(session, 'What would you like me to help with?', "Recommend companies|Find news and insights");
+            session.send('What would you like me to help with? Recommend companies or Find news and insights');
+            session.endDialog();
             }
     ]);
 };
