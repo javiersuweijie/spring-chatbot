e7d5ebee9b5730bdfcaad00c26a3e63355335ba4
Javier <javier.su.weijie@gmail.com>
2016-11-10 06:42:22 +0000

    changed some stuff

--- returning_user_nonews.js
+++ returning_user_nonews.js
@@ -1,12 +1,13 @@
 startup = function(bot, builder, asteroid) {
     bot.dialog('/returning_user_nonews', [
         function (session, results) {
-            builder.Prompts.choice(session, "Ok. How about looking at some companies similar to your previous search?", "Yes|No");
+            builder.Prompts.choice(session, "Ok. I also found some companies that might be interesting?", "Yes|No");
             },
             function (session, results) {
                 if (results.response.entity === 'Yes') {
 // this part is still static now, we can figure out how to code it later
-                    session.send("Displaying companies. 66% of investors with similar interests as you looked at these.")
+                    session.send("66% of the investors like you looked at these companies when you were away.")
+                    session.send("Displaying them now.")
                     session.endDialog();
                 } else if (results.response.entity === 'No') {
                     session.send("Ok. What would you like to find out today?");
